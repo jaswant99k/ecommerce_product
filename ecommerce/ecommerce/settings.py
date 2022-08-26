e8a17f2a83f50c8d01bf4ed1603ebb206361c53a
Jaswant <jaswantkumar999@gmail.com>
2022-08-27 00:39:16 +0530

    db changed

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -78,13 +78,26 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+ 	    'NAME': 'jaswant99k0$ecom',
+        'USER': 'jaswant99k0',
+        'PASSWORD': 'Ecom@123',
+        'HOST': 'jaswant99k0.mysql.pythonanywhere-services.com',
+        #'PORT': '5432',
+
+       
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
